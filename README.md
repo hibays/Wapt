385c9aa81e1e11282482063a147901a6c1775db2
hibays <12765444+hibays@user.noreply.gitee.com>
2026-03-17 13:21:39 +0800

    添加 .gitignore 文件，更新 README.md 以提供构建说明，重构 add_url.py 以支持命令行参数，修改 deepseek.rc 文件格式

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Wapt - Web Application Packaging Toolkit
 
 It helps to package web applications into a single executable file via [Webui](https://webui.me).
+
+## How to build
+
+1. Clone the repository
+
+	```bash
+	git clone https://github.com/hibays/Wapt.git
+	cd Wapt
+	```
+
+2. Directly build via CMake
+
+	```bash
+	cmake -B build -DCMAKE_BUILD_TYPE=Release
+	cmake --build build
+	```
+
+Then, you can find the executable file in the `build` directory.
+You can just run the executable file to start the web application.
+**That's it! Simple as that!**
+
+## How to add a new web application
+
+Just use the `add_url.py` script to add a new web application.
+
+```bash
+# Interactive mode
+python add_url.py
+
+# CLI mode
+python add_url.py --name myapp --url https://myapp.com --icon myapp.ico
+```
--- add_url.py
+++ add_url.py
@@ -1,5 +1,6 @@
-import os
 from pathlib import Path
+import argparse as ap
+import sys
 
 c_url_template = """
 #include "webui.h"
@@ -16,12 +17,6 @@ cmake_template = """
 add_executable({0} src/{1}.c)
 target_link_libraries({0} webui)
 
-if(WIN32 AND EXISTS icons/{1}.rc)
-	# linux cmake cant recognize .rc file
-	# NOTE: Use MSVC CMake instead of mingw version if build failed here
-	target_sources({0} PRIVATE icons/{1}.rc)
-endif()
-
 if(NOT CMAKE_BUILD_TYPE MATCHES "Deb")
 	if(MSVC)
 		target_link_options({0} PRIVATE /SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup)
@@ -31,29 +26,73 @@ if(NOT CMAKE_BUILD_TYPE MATCHES "Deb")
 endif()
 """
 
+cmake_icon_ext = """
+if(WIN32)
+	# linux cmake cant recognize .rc file
+	# NOTE: Use MSVC CMake instead of mingw version if build failed here
+	message(STATUS "Enabled {0} icon")
+	target_sources({0} PRIVATE icons/{1}.rc)
+endif()
+"""
+
 icon_template = """
-IDI_ICON1 ICON {}
+101 ICON {}
 """
 
 
-def main():
-	target_name = input("Enter the cmake target name (as is execution name): ")
-	cf_name = input("Enter the c file name (without extension): ")
-	url = input("Enter the URL: ")
-	icon = input("Enter the icon path (leave empty if no any): ")
+def add_url(target_name: str, cf_name: str, url: str, icon: str | None = None):
 
 	c_code = c_url_template % url
-	(Path("src") / f"{cf_name}.c").write_text(c_code)
+	Path("src", f"{cf_name}.c").write_text(c_code)
 
 	cmake_code = cmake_template.format(target_name, cf_name)
 	with open("CMakeLists.txt", "a") as f:
 		f.write(cmake_code)
+		if icon:
+			cmake_code = cmake_icon_ext.format(target_name, cf_name)
+			f.write(cmake_code)
 
-	if not icon:
-		return
+	if icon:
+		icon_code = icon_template.format(icon)
+		Path("icons", f"{target_name}.rc").write_text(icon_code)
+		print(f"Icon rc:\n{icon_code}")
 
-	icon_code = icon_template.format(icon)
-	(Path("icons") / f"{target_name}.rc").write_text(icon_code)
+
+def main():
+	if sys.argv.__len__() < 2:
+		print("-- Interactive mode")
+
+		target_name = input(
+		    "Enter the cmake target name (as is execution name): "
+		)
+		cf_name = input("Enter the c file name (without extension): ")
+		url = input("Enter the URL: ")
+		icon = input("Enter the icon path (leave empty if no any): ")
+
+	else:
+		print("-- CLI mode")
+		ap_parser = ap.ArgumentParser()
+		ap_parser.add_argument(
+		    "--name",
+		    type=str,
+		    required=True,
+		    help="The cmake target name (as is execution name)"
+		)
+		ap_parser.add_argument("--url", type=str, required=True, help="The URL")
+		ap_parser.add_argument(
+		    "--icon",
+		    type=str,
+		    default=None,
+		    help="The icon path (leave empty if no any)"
+		)
+		args = ap_parser.parse_args()
+
+		target_name = args.name
+		cf_name = args.name
+		url = args.url
+		icon = args.icon
+
+	return add_url(target_name, cf_name, url, icon)
 
 
 if __name__ == "__main__":
