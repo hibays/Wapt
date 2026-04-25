81506b30bd376117fc9b679af51389bc915565dc
hibays <12765444+hibays@user.noreply.gitee.com>
2026-04-25 23:38:29 +0800

    Add resource file for z.ai with application icon

---
+++ src/Z.ai/z.ai.c
@@ -0,0 +1,14 @@
+#include "webui.h"
+
+#define TARGET_URL "https://chat.z.ai/"
+
+int main() {
+	size_t my_window = webui_new_window();
+	webui_show(my_window, TARGET_URL);
+	webui_clean();
+	return 0;
+}
+
+#if defined(_MSC_VER)
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow) { return main(); }
+#endif
